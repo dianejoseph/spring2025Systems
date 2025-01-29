030878ac3b164d2bb46481348cb2eec5eb927de9
Diane Joseph <104699671+dianejoseph@users.noreply.github.com>
2025-01-29 18:42:55 +0000

    new change

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,15 @@
+
 fn main() {
-    println!("Hello, Diane");
+    // Converting a string to a number
+    let num = "25";
+    let num: u32 = num.parse()
+        .expect("Please provide a valid number!");
+    
+    println!("Parsed number: {}", num);
+    
+    // Further transformations
+    let num = num + 25;
+    let num = num * 2;
+    
+    println!("Final value of num: {}", num);
 }
