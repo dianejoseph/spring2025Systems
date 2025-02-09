d3b1a6133a84e6a0f46261c8fec659b2c6b30f16
Diane Joseph <104699671+dianejoseph@users.noreply.github.com>
2025-02-09 03:27:15 +0000

    commit again

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 fn main(){
-    let x = 3;
+    let number = 5;
 
-    if rem = x%3;
+    if rem = x % 3;
 
     if rem == 0 {
         println!("Divisible");
     } else if rem == 1 {
         println!("Remainder = {}", rem);
     }
-}
-
-fn main(){
-    let x = true;
-
-    match x {
-        true => println!("True");
-        
-    }
+} 
